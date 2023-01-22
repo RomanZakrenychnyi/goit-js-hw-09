8733329a7567b7bb7eb44c1cda908829625b53e4
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-01-22 16:29:23 +0200

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  start: document.querySelector('[data-start'),
+  stop: document.querySelector('[data-stop'),
+};
 
+const INTERVAL_DURATION = 1000;
+let intervalId = null;
+refs.stop.disabled = true;
+
+refs.start.addEventListener('click', onBtnClickStart);
+refs.stop.addEventListener('click', onBtnClickStop);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnClickStart() {
+    refs.start.disabled = true;
+    refs.stop.disabled = false;
+
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+        
+    }, INTERVAL_DURATION);
+}
+
+function onBtnClickStop() {
+    refs.stop.disabled = true;
+    refs.start.disabled = false;
+    clearInterval(intervalId);
+};
