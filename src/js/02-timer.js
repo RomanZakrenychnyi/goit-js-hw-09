5ef16b9549906c4be89a92f46ae1945a72e8b670
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-19 19:04:42 +0200

    commit task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,79 +1,79 @@
-import flatpickr from "flatpickr";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import flatpickr from "flatpickr";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-    btnStart: document.querySelector('button'),
-    timerDays: document.querySelector('[data-days]'),
-    timerHours: document.querySelector('[data-hours]'),
-    timerMinutes: document.querySelector('[data-minutes]'),
-    timerSeconds: document.querySelector('[data-seconds]')
-}
+// const refs = {
+//     btnStart: document.querySelector('button'),
+//     timerDays: document.querySelector('[data-days]'),
+//     timerHours: document.querySelector('[data-hours]'),
+//     timerMinutes: document.querySelector('[data-minutes]'),
+//     timerSeconds: document.querySelector('[data-seconds]')
+// }
 
-refs.btnStart.disabled = true;
-let timerId = null;
+// refs.btnStart.disabled = true;
+// let timerId = null;
 
-refs.btnStart.addEventListener('click', onTimerStart);
+// refs.btnStart.addEventListener('click', onTimerStart);
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
   
-  onClose(selectedDates) {
-      const todaysDate = new Date();
+//   onClose(selectedDates) {
+//       const todaysDate = new Date();
 
-      if (selectedDates[0] - todaysDate > 0) {
-        refs.btnStart.disabled = false;
-      } else {
-          refs.btnStart.disabled = true;
-          Notify.failure('Please choose a date in the future', {
-              timeout: 1500,
-              width: '400px',
-          });
-      }
-    },
-};
+//       if (selectedDates[0] - todaysDate > 0) {
+//         refs.btnStart.disabled = false;
+//       } else {
+//           refs.btnStart.disabled = true;
+//           Notify.failure('Please choose a date in the future', {
+//               timeout: 1500,
+//               width: '400px',
+//           });
+//       }
+//     },
+// };
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, 0);
-}
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, 0);
+// }
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
-}
+//   return { days, hours, minutes, seconds };
+// }
 
-function onTimerStart() {
-    const chosenDate = fp.selectedDates[0]    
+// function onTimerStart() {
+//     const chosenDate = fp.selectedDates[0]    
     
-    timerId = setInterval(() => {
-        const startTime = new Date();
-        const countdown = chosenDate - startTime;
-        refs.btnStart.disabled = true;
+//     timerId = setInterval(() => {
+//         const startTime = new Date();
+//         const countdown = chosenDate - startTime;
+//         refs.btnStart.disabled = true;
 
-        updateTimerFace(convertMs(countdown));
+//         updateTimerFace(convertMs(countdown));
         
-        if (countdown < 0) {
-            clearInterval(timerId);
-        }
-    }, 1000)
-}
+//         if (countdown < 0) {
+//             clearInterval(timerId);
+//         }
+//     }, 1000)
+// }
 
-function updateTimerFace({ days, hours, minutes, seconds }) {
-  refs.timerDays.textContent = addLeadingZero(days);
-  refs.timerHours.textContent = addLeadingZero(hours);
-  refs.timerMinutes.textContent = addLeadingZero(minutes);
-  refs.timerSeconds.textContent = addLeadingZero(seconds);
-}
+// function updateTimerFace({ days, hours, minutes, seconds }) {
+//   refs.timerDays.textContent = addLeadingZero(days);
+//   refs.timerHours.textContent = addLeadingZero(hours);
+//   refs.timerMinutes.textContent = addLeadingZero(minutes);
+//   refs.timerSeconds.textContent = addLeadingZero(seconds);
+// }
 
-const fp = flatpickr("#datetime-picker", options);
+// const fp = flatpickr("#datetime-picker", options);
