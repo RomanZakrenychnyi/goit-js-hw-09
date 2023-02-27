b48b4a58e1879437e0f6862fe39f91b763a19e50
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-27 19:53:54 +0000

    Deploying to gh-pages from @ RomanZakrenychnyi/goit-js-hw-09@57f59dc739f3038dfc6745fe2a1aaf778d63e8fe 🚀

---
+++ 01-color-switcher.ee3045b4.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;let d=null;e.addEventListener("click",(()=>{const a=e.closest("body");e.disabled=!0,t.disabled=!1,d=setInterval((()=>{a.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),750)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.ee3045b4.js.map
