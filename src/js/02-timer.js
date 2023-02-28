62eafeb498bbdce5ab38f9513378604223a542d4
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-28 11:27:29 +0200

    make all func as method of obj 'options'

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,48 +6,15 @@ const btnStart = document.querySelector('button');
 btnStart.disabled = true;
 let timerId = null;
 
-btnStart.addEventListener('click', onTimerStart);
+btnStart.addEventListener('click', onStart);
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  
-  onClose(selectedDates) {
-      const todaysDate = new Date();
 
-      if (selectedDates[0] - todaysDate > 0) {
-        btnStart.disabled = false;
-      } else {
-          btnStart.disabled = true;
-          Notify.failure('Please choose a date in the future', {
-              timeout: 1500,
-              width: '400px',
-          });
-      }
-    },
-};
-
-function pad(value) {
-  return `${value}`.padStart(2, 0);
-}
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-function onTimerStart() {
+  onStart() {
     const chosenDate = fp.selectedDates[0]    
     
     timerId = setInterval(() => {
@@ -61,13 +28,61 @@ function onTimerStart() {
             clearInterval(timerId);
         }
     }, 1000)
-}
+},
+  
+  onClose(selectedDates) {
+      const todaysDate = new Date();
 
-function updateTimerFace({ days, hours, minutes, seconds }) {
+      if (selectedDates[0] - todaysDate > 0) {
+        btnStart.disabled = false;
+      } else {
+          btnStart.disabled = true;
+          Notify.failure('Please choose a date in the future', {
+              timeout: 1500,
+              width: '400px',
+          });
+      }
+  },
+  
+  convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+  },
+  
+  convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+  },
+
+  updateTimerFace({ days, hours, minutes, seconds }) {
   document.querySelector('[data-days]').textContent = pad(days);
   document.querySelector('[data-hours]').textContent = pad(hours);
   document.querySelector('[data-minutes]').textContent = pad(minutes);
   document.querySelector('[data-seconds]').textContent = pad(seconds);
-}
+  },
+  
+  pad(value) {
+  return `${value}`.padStart(2, 0);
+  },
+
+};
 
 const fp = flatpickr("#datetime-picker", options);
